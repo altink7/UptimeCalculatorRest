47d6d641814a1fbe75c5a03040db545ae006bef1
altin <kelmendialtin48@gmail.com>
2022-06-10 21:33:06 +0200

    Unabhängigkeiten programmiert

--- src/main/java/fh/bwi/uptimecalculatorslm/UptimeCalcController.java
+++ src/main/java/fh/bwi/uptimecalculatorslm/UptimeCalcController.java
@@ -2,23 +2,26 @@ package fh.bwi.uptimecalculatorslm;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("calc/")
 public class UptimeCalcController {
 
-    @GetMapping("/calc")
-    public String greeting() {
+    @GetMapping("/{percent}")
+    public String calcCL(@PathVariable float percent) {
 
         //Hier Wert reingeben!!
-        int Value= 49;
+        float Value= percent; //percent löschen, dein Wert reingeben, mit percent beim / ( hier Wert reingeben)
         //Oben Wert eingeben beim Value
-        int Val=100-Value;
-        String sVal =String.valueOf(Val);
+
+        float Val=100-Value;
+        String sVal =String.valueOf("|"+Val+"|\n");
         return "Value:"+sVal+"====>"
-                +String.format(" %s %2d%s %2.0f%s %2.0f%s", "Daily:", 24*Val/100,"d" ,(24*(float)Val/100%1)*60-(24*(float)Val/100%1)*60%1,"m",(24*(float)Val/100%1)*60%1*60,"s");
+                +String.format(" %s %2f%s %2.0f%s %2.0f%s", "Daily:", 24*Val/100,"h" ,
+                (24*Val/100%1)*60-(24*Val/100%1)*60%1,"m",(24*Val/100%1)*60%160,"s");
     }
-
-
-
 }
+
